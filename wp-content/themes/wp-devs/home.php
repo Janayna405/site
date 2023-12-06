50e184460d32f05775cd8292a101f49feda8b756
Janayna@405 <janainabs21@outlook.com.br>
2023-12-06 17:40:22 -0300

    tamplate

--- wp-content/themes/wp-devs/home.php
+++ wp-content/themes/wp-devs/home.php
@@ -21,7 +21,7 @@
                                              <h2><?php the_title(); ?></h2>
                                              <div class="Meta-info"> 
                                                   <p> Posted in <?php echo get_the_date(); ?>by <?php 
-                                                  the_author_posts_link(); ?> </p>
+                                                 the_author_posts_link(); ?> </p>
                                                   <p>Categories: <?php the_category(  '  '); ?> </p>
                                                   <p> Tags: <?php the_tags('', ','); ?> </p>
                                             </div>
