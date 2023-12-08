0ad042ca5b659574b7dab7fc20d52cb32d1dd873
Janayna@405 <janainabs21@outlook.com.br>
2023-12-07 21:25:01 -0300

    tamplate.php

--- wp-content/themes/wp-devs/general-template.php
+++ wp-content/themes/wp-devs/general-template.php
@@ -1,5 +1,5 @@
 <?php 
-/*Template Nome: General Template
+/*Template Name: General Template
 */
 ?>
 <?php get_header();?>
@@ -7,28 +7,14 @@
     <div id="Content" class="site-content">
         <div id= "primary" class="content-area">
           <main id= "main" class= "site-main">
-            <section class="hero">
-                 Hero
-            </section>
-            <section class="services">
-                 Services
-            </section>
-            <section class= "home-blog">
                <div class="container">
-                    <div class="blog_items">
-
+                    <div class="general-template">
                          <?php 
                               if (have_posts()):
                                    while( have_posts()) : the_post();
                                    ?>
                                         <article>
-                                             <h2><?php the_title(); ?></h2>
-                                             <div class="Meta-info"> 
-                                                  <p> Posted in <?php echo get_the_date(); ?>by <?php 
-                                                  the_author_posts_link(); ?> </p>
-                                                  <p>Categories: <?php the_category(  '  '); ?> </p>
-                                                  <p> Tags: <?php the_tags('', ','); ?> </p>
-                                            </div>
+                                             <h1><?php the_title(); ?></h1>
                                             <?php the_content(); ?>
                                         </article>
                                      <?php
